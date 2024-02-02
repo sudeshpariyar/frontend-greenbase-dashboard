7d734004733a06a3a832b4f6db27133e34dd4285
sudeshpariyar <sudesh.bullet@gmail.com>
2024-02-02 16:40:27 +0800

    small fixes for the route

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RouterEvent } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'frontend-gb-task';
+
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.router.navigate(['/dashboard']);
+  }
 }
